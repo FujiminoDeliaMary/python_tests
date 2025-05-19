65836289fd8cbc2526972a5a75b9dfca2bd2d0b9
TSENE TABENGO <marilyne.tsenetabengo@cgi.com>
2025-05-19 12:02:50 +0200

    Initialisation

---
+++ main.utils.py
@@ -0,0 +1,14 @@
+def addition(a, b):
+    a + b
+
+def soustraction(a, b):
+    a - b
+
+def division(a, b):
+    if (b == 0):
+        raise ValueError("Division par 0 impossible")
+    return a / b
+
+def multiplication(a, b):
+    a * b
+
