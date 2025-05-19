308b21de7d9e06a7cd472bf1c2b9334969c36986
TSENE TABENGO <marilyne.tsenetabengo@cgi.com>
2025-05-19 12:58:11 +0200

    fix :fix ci

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ from main_utils import addition
 class TestUtils(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(addition(2,3), 5)
-        self.assertEqual(addition(1,-1), 2)
+        self.assertEqual(addition(1,-1), 0)
 
 
 
